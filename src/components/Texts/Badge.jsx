821aa4f5b6dd4ab92653002b840783af7e5b3268
Paulo Victor Silva <paulpbi16@gmail.com>
2024-11-29 09:45:50 -0300

    fix: badge width

--- src/components/Texts/Badge.jsx
+++ src/components/Texts/Badge.jsx
@@ -2,16 +2,16 @@ import Proptypes from "prop-types";
 
 const Badge = ({ badges }) => {
   return (
-    <>
+    <div className="flex w-full flex-wrap gap-1">
       {badges.map((badgeItems) => (
         <span
           key={badgeItems}
-          className="bg-secondary_100 text-secondary_200 flex items-center justify-around gap-2 rounded px-2 py-1 text-[12px] font-bold uppercase tracking-wider"
+          className="flex items-center justify-around gap-2 rounded bg-secondary_100 px-2 py-1 text-[12px] font-bold uppercase tracking-wider text-secondary_200"
         >
           {badgeItems}
         </span>
       ))}
-    </>
+    </div>
   );
 };
 
